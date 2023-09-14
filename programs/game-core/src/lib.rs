a2d034b49cfc29deaddb303fe8dda1e33a585c7c
ellagoris <eduardofidera@gmail.com>
2023-09-14 18:15:59 -0300

    add palace and token minting

--- programs/game-core/src/lib.rs
+++ programs/game-core/src/lib.rs
@@ -1,15 +1,75 @@
 use anchor_lang::prelude::*;
 
+use anchor_spl::{ token::{ Token, Mint, MintTo, mint_to, TokenAccount } };
+
 declare_id!("9LqUvkM7zkVqpYypCRsuh5KitHbZZFrcfwkRVgirnnUf");
+
+#[account]
+pub struct Palace {
+    pub level: i8,
+}
 
 #[program]
 pub mod game_core {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.palace.set_inner(Palace {
+            level: 1,
+        });
+
+        Ok(())
+    }
+
+    pub fn upgrade_palace(ctx: Context<Upgrade>) -> Result<()> {
+        ctx.accounts.palace.level = ctx.accounts.palace.level + 1;
+
+        Ok(())
+    }
+
+    pub fn mint_tokens(ctx: Context<Auth>, amount: u64) -> Result<()> {
+        // Create the MintTo struct for our context
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.destination_ata.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the CpiContext we need for the request
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        // Execute anchor's helper function to mint tokens
+        mint_to(cpi_ctx, amount)?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8, seeds = [signer.key().as_ref()], bump)]
+    pub palace: Account<'info, Palace>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Upgrade<'info> {
+    #[account(mut, seeds = [signer.key().as_ref()], bump)]
+    pub palace: Account<'info, Palace>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Auth<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub destination_ata: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+}
--- tests/game-core.ts
+++ tests/game-core.ts
@@ -1,16 +1,81 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { GameCore } from "../target/types/game_core";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { GameCore } from "../target/types/game_core"
+import { getAssociatedTokenAddress } from "@solana/spl-token"
 
 describe("game-core", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.GameCore as Program<GameCore>;
+  const program = anchor.workspace.GameCore as Program<GameCore>
 
   it("Is initialized!", async () => {
+    // get palace PDA
+    const palaceAddress = anchor.web3.PublicKey.findProgramAddressSync(
+      [program.provider.publicKey.toBytes()],
+      anchor.workspace.GameCore.programId
+    )[0]
+
+    console.log(palaceAddress.toString())
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        palace: palaceAddress,
+      })
+      .rpc()
+    console.log("Your transaction signature", tx)
+
+    // fetch the palace account
+    const palace = await program.account.palace.fetch(palaceAddress)
+    console.log(palace)
+  })
+
+  it("Can be upgraded", async () => {
+    // get palace PDA
+    const palaceAddress = anchor.web3.PublicKey.findProgramAddressSync(
+      [program.provider.publicKey.toBytes()],
+      anchor.workspace.GameCore.programId
+    )[0]
+
+    console.log(palaceAddress.toString())
+    // Add your test here.
+    const tx = await program.methods
+      .upgradePalace()
+      .accounts({
+        palace: palaceAddress,
+      })
+      .rpc()
+
+    console.log("Your transaction signature", tx)
+
+    // fetch the palace account
+    const palace = await program.account.palace.fetch(palaceAddress)
+    console.log(palace)
+  })
+
+  it("Can mint tokens", async () => {
+    // get palace PDA
+    const mint = new anchor.web3.PublicKey(
+      "4zfn53iuTbnQDsJDtJvnuhYaqC5JaGSPJUSvaG4zZT6u"
+    )
+    const destination = new anchor.web3.PublicKey(
+      "7x4JZgW2oeAcra18oMC7Tudu9h6D5cYMJnjy8AbubBVW"
+    )
+
+    const ata = await getAssociatedTokenAddress(mint, destination)
+
+    console.log(destination.toString())
+
+    // Add your test here.
+    const tx = await program.methods
+      .mintTokens(new anchor.BN(1e9))
+      .accounts({
+        mint,
+        destinationAta: ata,
+      })
+      .rpc()
+
+    console.log("Your transaction signature", tx)
+  })
+})
